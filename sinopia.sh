1660467cdba25e0ace741d9948fbd46f22df91e6
Ryan Graham <ryan@strongloop.com>
2015-09-12 18:08:51 -0700

    add support for pre-defining a user

--- sinopia.sh
+++ sinopia.sh
@@ -9,21 +9,18 @@ auth:
   htpasswd:
     file: ./htpasswd
     max_users: ${MAX_USERS:-1}
-# a list of other known repositories we can talk to
 uplinks:
   upstream:
     url: $(npm config get registry)
 packages:
-  '@*/*':
-    # scoped packages
-    access: \$all
-    publish: \$authenticated
   '*':
     access: \$all
     publish: \$authenticated
     proxy: upstream
 logs:
-  - {type: stdout, format: pretty, level: http}
+  - {type: stdout, format: pretty, level: info}
 EOYML
+
+echo "${NPM_USER:-admin}:{PLAIN}${NPM_PASSWORD:-admin}" > ./htpasswd
 
 exec ./node_modules/.bin/sinopia --config ./config.yml
