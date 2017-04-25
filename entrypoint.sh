21e13f696ff7acb0efa2158bbd5f512428c860dd
Ryan Graham <r.m.graham@gmail.com>
2017-04-25 13:47:06 -0700

    initial nginx/openresty based proxy
    Rewrites URLs in response bodies so that fetched package metadata has
    updated tarball URLs that point back to this server.
    
    Does not support any publishing or fine-grained cache control.

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# ephemeral-npm env vars supported:
+# [x] npm_config_registry
+# [ ] MAX_BODY_SIZE
+# [ ] MAX_USERS
+# [ ] MAXAGE
+# [ ] TIMEOUT
+# [ ] NPM_SECRET
+# [ ] NPM_USER
+# [ ] NPM_PASSWORD
+
+export npm_config_registry=${npm_config_registry:-https://registry.npmjs.org}
+
+# necessary because nginx requires a resolver when upstreams are dynamic
+dnsmasq --listen-address=127.0.0.1 --user=root
+
+exec /usr/local/openresty/bin/openresty -c /nginx.conf $*
