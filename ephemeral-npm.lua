992a041273b47f82fbf8ffc4d792935a7f711237
Ryan Graham <r.m.graham@gmail.com>
2017-05-11 12:55:07 -0700

    nginx: consolidate per-request Lua usage
    Instead of invoking Lua at multiple parts of the request lifecycle,
    consolidate to as few phases as possible.
    
    As a bonus, this also simplifies the code.

--- ephemeral-npm.lua
+++ ephemeral-npm.lua
@@ -22,6 +22,7 @@ function _M.getPackage()
     local uri = ngx.var.uri
     local meta = ngx.shared.npmMeta
     local body = meta:get(uri)
+    local base = ngx.var.scheme .. '://' .. ngx.var.http_host
     -- yep, our own shared memory cache implementation :-/
     if body == nil then
         ngx.var.ephemeralCacheStatus = 'MISS'
@@ -39,17 +40,9 @@ function _M.getPackage()
     else
         ngx.var.ephemeralCacheStatus = 'HIT'
     end
+    body = string.gsub(body, _M.hostPattern, base)
     ngx.header["Content-Length"] = #body
     ngx.print(body)
-end
-
-function _M.filterPackageBody()
-    local npmConfig = ngx.shared.npmConfig
-    local upstream = npmConfig:get('npm_upstream_pattern')
-    -- need to construct URL because we may be proxying http<->https
-    local base = ngx.var.scheme .. '://' .. ngx.var.http_host
-    -- ngx.log(ngx.ERR, "Modifying JSON of " .. ngx.var.uri .. " to replace '" .. upstream .. "' with '" .. base .. "'")
-    ngx.arg[1] = string.gsub(ngx.arg[1], upstream, base)
 end
 
 return _M
